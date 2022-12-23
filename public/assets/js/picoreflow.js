d3f13b3cfe49ae4a7c602b5329dde0c42e3a01a4
Brian Gillespie <brian@mcneel.com>
2022-12-22 21:03:00 -0800

    Add saving of kiln log

--- public/assets/js/picoreflow.js
+++ public/assets/js/picoreflow.js
@@ -13,6 +13,7 @@ var time_scale_long = "Seconds";
 var temp_scale_display = "C";
 var kwh_rate = 0.26;
 var currency_type = "EUR";
+var run_log = []
 
 var protocol = 'ws:';
 if (window.location.protocol == 'https:') {
@@ -454,6 +455,52 @@ function getOptions()
 
 }
 
+function saveLog()
+{
+    exportToCsv('kiln.csv', run_log);
+}
+
+function exportToCsv(filename, rows) {
+    var processRow = function (row) {
+        var finalVal = '';
+        for (var j = 0; j < row.length; j++) {
+            var innerValue = row[j] === null ? '' : row[j].toString();
+            if (row[j] instanceof Date) {
+                innerValue = row[j].toLocaleString();
+            };
+            var result = innerValue.replace(/"/g, '""');
+            if (result.search(/("|,|\n)/g) >= 0)
+                result = '"' + result + '"';
+            if (j > 0)
+                finalVal += ',';
+            finalVal += result;
+        }
+        return finalVal + '\n';
+    };
+
+    var csvFile = '';
+    for (var i = 0; i < rows.length; i++) {
+        csvFile += processRow(rows[i]);
+    }
+
+    var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+    if (navigator.msSaveBlob) { // IE 10+
+        navigator.msSaveBlob(blob, filename);
+    } else {
+        var link = document.createElement("a");
+        if (link.download !== undefined) { // feature detection
+            // Browsers that support HTML5 download attribute
+            var url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", filename);
+            link.style.visibility = 'hidden';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    }
+}
+
 
 
 $(document).ready(function()
@@ -502,8 +549,8 @@ $(document).ready(function()
 
         ws_status.onmessage = function(e)
         {
-            console.log("received status data")
-            console.log(e.data);
+            //console.log("received status data")
+            //console.log(e.data);
 
             x = JSON.parse(e.data);
             if (x.type == "backlog")
@@ -531,6 +578,9 @@ $(document).ready(function()
 
                 if (state!=state_last)
                 {
+                    if (state == 'RUNNING' && state_last == 'IDLE') {
+                        run_log = [['time','target','temp','heat','pid.error','pid.errorDelta','pid.p','pid.i','pid.d','pid.kp','pdi.ki','pid.kd']]
+                    }
                     if(state_last == "RUNNING")
                     {
                         if (state.includes("ERROR")) {
@@ -566,6 +616,21 @@ $(document).ready(function()
 
                 if(state=="RUNNING")
                 {
+                    run_log.push([
+                        x.pidstats.time,
+                        x.target,
+                        x.temperature,
+                        x.heat,
+                        x.pidstats.err,
+                        x.pidstats.errDelta,
+                        x.pidstats.p,
+                        x.pidstats.i,
+                        x.pidstats.d,
+                        x.pidstats.kp,
+                        x.pidstats.ki,
+                        x.pidstats.kd
+                    ]);
+
                     $("#nav_start").hide();
                     $("#nav_stop").show();
 
